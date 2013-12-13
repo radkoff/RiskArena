d0d38a380d9e2adf2752e98e38f5cc0a176d34f4
Evan Radkoff <eradkoff12@gmail.com>
2013-12-12 21:15:23 -0600

    GameStats calculates occupation stats and army totals

--- src/riskarena/riskbots/evaluation/GameStats.java
+++ src/riskarena/riskbots/evaluation/GameStats.java
@@ -1,5 +1,7 @@
 package riskarena.riskbots.evaluation;
 
+import java.util.ArrayList;
+
 import riskarena.CountryInfo;
 import riskarena.GameInfo;
 import riskarena.PlayerInfo;
@@ -7,8 +9,11 @@ import riskarena.World;
 
 public class GameStats {
 	private GameInfo game;
-	private CountryInfo countries[];
-	private World world;
+	private CountryInfo countries[];	// Stored at the class level so individual calculation
+										// methods don't need to have fresh copies made.
+	private PlayerInfo players[];
+	private World world;				// Stored at the class level and only set once because
+										// this never changes (mere adjacency info)
 	
 	/*
 	 * Ratings that represent a score for each continent, using only
@@ -21,23 +26,41 @@ public class GameStats {
 	 *	If no one does, the int will be -1;
 	 */
 	private int continentOwnership[];
-	
+	/*		Number of armies each player has on the board		*/
+	private int armies[];
+	/*		Total number of armies on the board			*/
+	private int totalArmies;
+	/*		Number of territories occupied by a given player ID		*/
+	private int occupationCounts[];
+	/*		A list of country IDs belonging to this player		*/
+	private ArrayList<Integer> myCountries;
 	
 	public GameStats(GameInfo initial) {
 		game = initial;
 		world = game.getWorldInfo();	// Only grab this once, it never changes
-		rateContinents();
+		rateContinents();				// Only rate these once, they never change
 	}
 	
+	//	Alerts GameStats that the world has changed, so reload and recalculate!
 	public void refresh() {
 		countries = game.getCountryInfo();
+		players = game.getPlayerInfo();
 		calculate();
 	}
 	
+	// Called to re-calculate all stat variables.
+	// Assumes all state variables like "countries" are up-to-date
 	private void calculate() {
 		calculateContentOwnership();
+		calculateArmyTotals();
+		calculateOccupationStats();
 	}
 	
+	/*
+	 * Responsible for filling in the continentOwnership array.
+	 * Each number is the player ID of someone that owns the entire continent.
+	 * If no one does, the int will be -1;
+	 */
 	private void calculateContentOwnership() {
 		continentOwnership = new int[game.getContinentBonuses().length];
 		// Initialize each content ownership to -2, which indicates the
@@ -58,7 +81,50 @@ public class GameStats {
 		}
 	}
 	
+	/*
+	 * Responsible for filling in the armies array with army total for each player,
+	 * as well as the totalArmies int.
+	 */
+	private void calculateArmyTotals() {
+		armies = new int[players.length];
+		totalArmies = 0;
+		for(int i=0; i<countries.length; i++) {
+			if(countries[i].isTaken()) {
+				armies[countries[i].getPlayer()] += countries[i].getArmies();
+				totalArmies += countries[i].getArmies();
+			}
+		}
+	}
+	
+	/*
+	 * Responsible for filling the occupationTotals array and myCountries ArrayList
+	 * occupationTotals[i] represents the number of territories occupied by the player with ID i
+	 * myCountries is a list of the country ID's belonging to this player.
+	 */
+	private void calculateOccupationStats() {
+		occupationCounts = new int[players.length];
+		myCountries = new ArrayList<Integer>();
+		for(int i=0; i<countries.length; i++) {
+			if(countries[i].isTaken()) {
+				occupationCounts[countries[i].getPlayer()]++;
+				if(countries[i].getPlayer() == game.me())
+					myCountries.add(new Integer(i));
+			}
+		}
+	}
+	
+	/* Responsible for filling the continentRatings array with scores for each
+	 * continent they reflect how "good" they are. These use only
+	 * state-independent info: #territories, #borders, and the continent army bonus 
+	 */
 	private void rateContinents() {
 		// TODO
 	}
+	
+	/*********************** GETTERS ****************************/
+	
+	public int[] getContinentOwnership() {
+		return continentOwnership;
+	}
+	
 }
